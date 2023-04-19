9730ceb4d7ded50c76e3adbe222fe0d5b5aea687
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-04-19 15:14:51 +0300

    feat : add tab auctions

--- src/views/pages/AuctionsPage.vue
+++ src/views/pages/AuctionsPage.vue
@@ -1,1 +1,14 @@
-<template></template>
+<template>
+  <bread-cumb route_name="Auctions" />
+  <Tab-auctions />
+</template>
+<script>
+import BreadCumb from "../../components/UI/menu/BreadCumb.vue";
+import TabAuctions from "../../Model/auctions/TabAuctions.vue";
+export default {
+  components: {
+    "Tab-auctions": TabAuctions,
+    BreadCumb,
+  },
+};
+</script>
