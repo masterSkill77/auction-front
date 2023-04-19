a6bb9f771c141fd539f0a71d79b2af4e24f7972c
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-04-19 12:41:14 +0300

    feat : use countdown

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import VueCountdown from "@chenfengyuan/vue-countdown";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,5 +11,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.component(VueCountdown.name, VueCountdown);
 
 app.mount("#app");
