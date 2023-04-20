c67c677e34f5daa33be2d5aed91887de3f183392
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-04-20 21:07:05 +0300

    feat : add details about auction

--- src/Model/auctions/AuctionCard.vue
+++ src/Model/auctions/AuctionCard.vue
@@ -3,11 +3,7 @@
     <div class="row">
       <div class="col-5 col-lg-5">
         <div class="card m-auto text-center">
-          <img
-            class="card-img-top w-50 mt-5 mx-auto"
-            :src="auction.nft.image_uri"
-            alt="Card image cap"
-          />
+          <img class="card-img-top w-50 mt-5 mx-auto" :src="auction.nft.image_uri" alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">{{ auction.nft.title }}</h5>
             <p class="card-text">
@@ -17,12 +13,9 @@
             <a href="#" class="btn btn-primary">Make a bid</a>
           </div>
           <div class="card-footer text-muted">
-            <vue-countdown
-              :time="
+            <vue-countdown :time="
                 new Date(auction.end_date).getTime() - new Date().getTime()
-              "
-              v-slot="{ days, hours, minutes, seconds }"
-            >
+              " v-slot="{ days, hours, minutes, seconds }">
               <b>
                 Time Remaining：{{ days }} days, {{ hours }} hours,
                 {{ minutes }} minutes, {{ seconds }} seconds.
@@ -35,99 +28,73 @@
         <div class="cryptos-tabs-content">
           <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item">
-              <a
-                class="nav-link active"
-                id="tab--1"
-                data-toggle="tab"
-                href="#tab1"
-                role="tab"
-                aria-controls="tab1"
-                aria-selected="false"
-                >Owner</a
-              >
+              <a class="nav-link active" id="tab--1" data-toggle="tab" href="#tab1" role="tab" aria-controls="tab1"
+                aria-selected="false">Owner</a>
             </li>
             <li class="nav-item">
-              <a
-                class="nav-link"
-                id="tab--2"
-                data-toggle="tab"
-                href="#tab2"
-                role="tab"
-                aria-controls="tab2"
-                aria-selected="false"
-                >Details</a
-              >
+              <a class="nav-link" id="tab--2" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2"
+                aria-selected="false">Details</a>
             </li>
             <li class="nav-item">
-              <a
-                class="nav-link"
-                id="tab--3"
-                data-toggle="tab"
-                href="#tab3"
-                role="tab"
-                aria-controls="tab3"
-                aria-selected="true"
-                >Bids</a
-              >
+              <a class="nav-link" id="tab--3" data-toggle="tab" href="#tab3" role="tab" aria-controls="tab3"
+                aria-selected="true">Bids</a>
             </li>
           </ul>
 
           <div class="tab-content mb-100" id="myTabContent">
-            <div
-              class="tab-pane fade  show active"
-              id="tab1"
-              role="tabpanel"
-              aria-labelledby="tab--1"
-            >
+            <div class="tab-pane fade  show active" id="tab1" role="tabpanel" aria-labelledby="tab--1">
               <div class="cryptos-tab-content">
                 <!-- Tab Text -->
                 <div class="cryptos-tab-text">
                   <p>
                     <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Name : {{ auction.owner.name + " " + auction.owner.lastname }}</li>
-                    <li class="list-group-item">Username : {{ auction.owner.username }}</li>
-                    <li class="list-group-item">Email : {{ auction.owner.email }}</li>
-                  </ul>
+                      <li class="list-group-item">Name : {{ auction.owner.name + " " + auction.owner.lastname }}</li>
+                      <li class="list-group-item">Username : {{ auction.owner.username }}</li>
+                      <li class="list-group-item">Email : {{ auction.owner.email }}</li>
+                    </ul>
                   </p>
                 </div>
               </div>
             </div>
-            <div
-              class="tab-pane fade"
-              id="tab2"
-              role="tabpanel"
-              aria-labelledby="tab--2"
-            >
+            <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab--2">
               <div class="cryptos-tab-content">
                 <!-- Tab Text -->
                 <div class="cryptos-tab-text">
                   <p>
                     <ul class="list-group list-group-flush">
-                      <li class="list-group-item">Start price : {{ auction.start_price }}</li>
-                      <li class="list-group-item">Current bid : {{ auction.current_bid }}</li>
+                      <li class="list-group-item">Start price : $ {{ auction.start_price }}</li>
+                      <li class="list-group-item">Current bid : $ {{ auction.current_bid }}</li>
                       <li class="list-group-item">Start date : {{ auction.start_date }}</li>
                     </ul>
                   </p>
                 </div>
               </div>
             </div>
-            <div
-              class="tab-pane fade"
-              id="tab3"
-              role="tabpanel"
-              aria-labelledby="tab--3"
-              >
+            <div class="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="tab--3">
               <div class="cryptos-tab-content">
                 <!-- Tab Text -->
                 <div class="cryptos-tab-text">
                   <p>
-                    {{ auction.bids }}
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">User</th>
+                          <th scope="col">Amount ($)</th>
+                          <th scope="col">Date</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="bid in auction.bids" :key="bid.in">
+                          <th scope="row">{{ bid.id }}</th>
+                          <td>{{ bid.bidder.name + " " + bid.bidder.lastname }}</td>
+                          <td>{{  bid.bid_amount  }}</td>
+                          <td>{{  bid.created_at  }}</td>
+                        </tr>
 
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item">Start price : {{ auction.start_price }}</li>
-                      <li class="list-group-item">Current bid : {{ auction.current_bid }}</li>
-                      <li class="list-group-item">Start date : {{ auction.start_date }}</li>
-                    </ul>
+                      </tbody>
+                    </table>
+
                   </p>
                 </div>
               </div>
@@ -139,23 +106,33 @@
   </section>
 </template>
 <script>
-import { defineComponent, onBeforeMount, ref } from "vue";
-import { useRoute } from "vue-router";
-import { useAuctionStore } from "../../stores/auction";
+  import {
+    defineComponent,
+    onBeforeMount,
+    ref
+  } from "vue";
+  import {
+    useRoute
+  } from "vue-router";
+  import {
+    useAuctionStore
+  } from "../../stores/auction";
 
-export default defineComponent({
-  setup() {
-    const auction = ref({ nft: {} });
-    const route = useRoute();
-    const auctionId = route.params.auctionId;
+  export default defineComponent({
+    setup() {
+      const auction = ref({
+        nft: {}
+      });
+      const route = useRoute();
+      const auctionId = route.params.auctionId;
 
-    onBeforeMount(async () => {
-      auction.value = await useAuctionStore().fetchAuction(auctionId);
-    });
+      onBeforeMount(async () => {
+        auction.value = await useAuctionStore().fetchAuction(auctionId);
+      });
 
-    return {
-      auction,
-    };
-  },
-});
+      return {
+        auction,
+      };
+    },
+  });
 </script>
